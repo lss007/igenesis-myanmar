da2fc1891125316d6e597e5a392bf18d0f9e9d94
Lucky Saggu <lss007@yahoo.com>
2022-08-29 13:00:06 +0530

    view messages fixed

--- resources/views/backend/contacts/index.blade.php
+++ resources/views/backend/contacts/index.blade.php
@@ -43,14 +43,14 @@
                 @foreach($get_messages as $keys => $value)
                 <tr>
                   <th scope="row" width="5%">{{$keys+1}}</th>
-                  <td width="20%"> {{$value->name}}</td>
+                  <td width="10%"> {{$value->name}}</td>
                   <td width="10%"> {{$value->email}}</td>
                   <td width="15%">{{$value->subject}}</td>
                   <td width="30%">
                  
                     {{Str::limit(	$value->message,30,$end='....')}}
                   </td>
-                  <td>
+                  <td width="25%">
                     {{-- view Message --}}
                     <a href="{{route('view.contact.messages',$value->id)}} " class="btn btn-primary">
                         <i class="bi bi-eye"></i>
