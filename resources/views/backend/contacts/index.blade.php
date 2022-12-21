d77d3d58fbbe1a316216155fde58cb7819dda7c5
Lucky Saggu <lss007@yahoo.com>
2022-12-21 13:47:20 +0530

    created at addded

--- resources/views/backend/contacts/index.blade.php
+++ resources/views/backend/contacts/index.blade.php
@@ -43,7 +43,10 @@
                 @foreach($get_messages as $keys => $value)
                 <tr>
                   <th scope="row" width="5%">{{$keys+1}}</th>
-                  <td width="10%"> {{$value->name}}</td>
+                  <td width="10%"> {{$value->name}}
+<br>
+{{Carbon\Carbon::parse($value->created_at)}}
+                  </td>
                   <td width="10%"> {{$value->email}}</td>
                   <td width="15%">{{$value->subject}}</td>
                   <td width="30%">
