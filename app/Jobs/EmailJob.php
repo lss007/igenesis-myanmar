bfa07f82ba10d8666c51842f677ad944568638b4
Lucky Saggu <lss007@yahoo.com>
2023-01-19 15:04:18 +0530

    email changed

--- app/Jobs/EmailJob.php
+++ app/Jobs/EmailJob.php
@@ -32,11 +32,11 @@ class EmailJob implements ShouldQueue
     {
         $email = new Emailsend($this->emaildata);
 
-        // Mail::to('sb@genesismyanmar.com')->send($email);
+       
         Mail::to('hpalss007@gmail.com')->send($email);
 
         
-        // Mail::to('info@genesismyanmar.com')->send($email);
+   
 
         
     }
