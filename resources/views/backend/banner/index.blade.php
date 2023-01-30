3e47b30f535f6a2d00639465670b425529b77f8e
lss007 <lss007@yahoo.com>
2023-01-30 17:09:35 +0530

    banner updated

--- resources/views/backend/banner/index.blade.php
+++ resources/views/backend/banner/index.blade.php
@@ -34,30 +34,22 @@
                     <h5 class="modal-title" id="exampleModalLabel">Add Banner</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
-                  <div class="modal-body">
-                    <form  method="post" action="{{route('store_all_banner')}}" enctype="multipart/form-data"> 
-                      @csrf
-                      <!-- <div class="mb-3">
-                        <label for="title" class="col-form-label">Title:</label>
-                        <input type="text" class="form-control"  name="title"  placeholder="Title">
-                        @error('title')
-                        <span class=" text-danger">{{ $message }}</span>
-                        @enderror
-                      </div> -->
-                      <div class="col-12">
+                    <div class="modal-body">
+                        <form  method="post" action="{{route('store_all_banner')}}" enctype="multipart/form-data"> 
+                        @csrf
+                        <div class="col-12">
                             <label for="Name" class="form-label"> Banner</label>
                             <input type="file" class="form-control" name="image" accept="image/*" onchange="document.getElementById('output').src = window.URL.createObjectURL(this.files[0])">
-
-                     </div>
-                    <div class="col-12">
+                        </div>
+                        <div class="col-12">
                             <img src="{{asset('no_image.jpg')}}" alt="" id="output" width="100">                                        
+                        </div>
                     </div>
-                  </div>
                      <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         <button type="submit" class="btn btn-primary">Save  </button>
-                  </div>
-                </form>
+                     </div>
+                 </form>
                 </div>
               </div>
             </div>
@@ -82,7 +74,7 @@
                 @foreach($get_banner as $keys => $value)
                 <tr>
                   <th scope="row" width="5%">{{$value->order_no}}</th>
-                  <td width="20%"> {{Str::limit(	$value->title,20,$end='....')}}<br> 
+                  <td width="20%"> {{Str::limit($value->title,20,$end='....')}}<br> 
                  </td>
                   <td width="20%" class="text-center">
                     <img src="{{(isset($value->image))   ? asset('assets/banner/'.$value->image) :asset('no_image.jpg')}}" 
@@ -104,40 +96,35 @@
                         <a  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#largeModal{{$value->id}}">
                             <i class="bi bi-eye"></i>
                         </a>
-                        <div class="modal fade" id="largeModal{{$value->id}}" tabindex="-1">
-                            <div class="modal-dialog modal-lg">
-                                <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title"> View Banner</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="row">
-                                    <div class="col-md-12">
-                                        <h5> Title:</h5>
-                                        <p> {!! isset($value->title) ? html_entity_decode($value->title) : ""!!}</p>                       
-                                        <h5> Image:</h5>
-                                        <img class=" p-3" src="{{asset('assets/banner/'.$value->image)}}" alt="{{$value->title}}" width="200">
+                            <div class="modal fade" id="largeModal{{$value->id}}" tabindex="-1">
+                                <div class="modal-dialog modal-lg">
+                                    <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title"> View Banner</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="row">
+                                        <div class="col-md-12 text-center">
+                                      
+                                            <h5> Banner Image:</h5>
+                                            <img class=" p-3" src="{{asset('assets/banner/'.$value->image)}}" alt="{{$value->title}}" width="400">
+                                        </div>
+                                        </div>
                                     </div>
                                     </div>
-                                   
-                                </div>
                                 </div>
                             </div>
-                        </div>
-                   <!-- End Large Modal-->
-                   <!-- edit model  -->
-                    <a  class="btn btn-primary" href="{{route('edit.banner',$value->id)}}">
-                          <i class="bi bi-pencil"></i>
-                    </a>
-
-                       <!-- edit model  -->
-                      <a href="{{route('delete_banner',$value->id)}}" class="btn btn-primary">
-                      <i class="bi bi-trash"></i>
-                    </a>
-                </td>
-                </tr>
-                @endforeach
+                          <!-- End Large Modal-->
+                            <a  class="btn btn-primary" href="{{route('edit.banner',$value->id)}}">
+                                <i class="bi bi-pencil"></i>
+                            </a>
+                            <a href="{{route('delete_banner',$value->id)}}" class="btn btn-primary">
+                                <i class="bi bi-trash"></i>
+                            </a>
+                        </td>
+                    </tr>
+                 @endforeach
                 @endif
               </tbody>
             </table>
